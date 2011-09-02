0aff2a70cbad0180b35349202d20088376d6879e
Brandon Satrom <brsatrom@microsoft.com>
2011-09-02 10:28:26 -0500

    Added custom behaviors and FluentHtml5ViewPage

---
+++ FluentHTML5/Behaviors/RangeBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MvcContrib.FluentHtml.Behaviors;
+using MvcContrib.FluentHtml.Elements;
+using MvcContrib.FluentHtml.Html;
+
+namespace FluentHtml5.Behaviors
+{
+    public class RangeBehavior : IBehavior<IMemberElement>
+    {
+        public void Execute(IMemberElement element)
+        {
+            var attribute = element.GetAttribute<RangeAttribute>();
+            if (attribute != null)
+            {
+                element.SetAttr(HtmlAttribute.Min, attribute.Minimum);
+                element.SetAttr(HtmlAttribute.Max, attribute.Maximum);
+            }
+        }
+    }
+}
---
+++ FluentHTML5/ViewPages/FluentHtml5ViewPage.cs
@@ -0,0 +1,11 @@
+﻿using FluentHtml5.Behaviors;
+using MvcContrib.FluentHtml;
+
+namespace FluentHtml5.ViewPages
+{
+    public class FluentHtml5ViewPage<T> : ModelViewPage<T> where T : class 
+    {
+        public FluentHtml5ViewPage()
+            : base(new RegularExpressionBehavior(), new RangeBehavior(), new RequiredBehavior()){}
+    }
+}
